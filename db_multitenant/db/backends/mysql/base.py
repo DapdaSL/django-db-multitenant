fd7cd0a84d2fbe15f7fed1c0663268ccd6c4983f
Antonio Gutiérrez <agutierrez@dapda.com>
2016-11-16 09:19:21 +0100

    Enable again cache with exception control.

--- db_multitenant/db/backends/mysql/base.py
+++ db_multitenant/db/backends/mysql/base.py
@@ -74,7 +74,10 @@ class DatabaseWrapper(WRAPPED_BACKEND.DatabaseWrapper):
             cursor.execute('USE `%s`;' % dbname)
             time_ms = int((time.time() - start_time) * 1000)
             LOGGER.debug('Applied dbname `%s` in %s ms' % (dbname, time_ms))
-            #connection.mt_dbname = dbname
+            try:
+                connection.mt_dbname = dbname
+            except:
+                pass
 
         return cursor
 
